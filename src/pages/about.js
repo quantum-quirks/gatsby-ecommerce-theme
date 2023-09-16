53d2e2f2d48d60f6588f402180ca5b0b28d963c6
Scalabli <s.gerrishon@gmail.com>
2023-09-16 04:20:51 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -27,7 +27,7 @@ const AboutPage = (props) => {
         <Hero
           maxWidth={'900px'}
           image={'/about.png'}
-          title={`Sydney \n A British brand since 1860`}
+          title={`Peritia\n A British brand`}
         />
 
         <div className={styles.navContainer}>
