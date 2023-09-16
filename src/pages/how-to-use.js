534f237c5914cc2825ec82b9280f941f3dee38ca
Scalabli <s.gerrishon@gmail.com>
2023-09-16 04:58:47 -0400

    Update how-to-use.js

--- src/pages/how-to-use.js
+++ src/pages/how-to-use.js
@@ -32,11 +32,11 @@ const HowToUsePage = (props) => {
         </div>
         <Container size={'large'} spacing={'min'}>
           <div className={styles.content} style={{ paddingTop: '80px' }}>
-            <h3>Built By Matter.</h3>
+            <h3>Built By Scalabli.</h3>
             <div id="#builtBy" ref={builtRef}>
               <p>
                 This theme is proudly brought to you by the team at{' '}
-                <Button target={true} href="https://matterdesign.com.au/">
+                <Button target={true} href="https://www.linkedin.com/company/scalabli">
                   Matter Design & Digital
                 </Button>{' '}
                 (Matter.).
